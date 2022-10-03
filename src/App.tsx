124aad47868dcaa6b631dd8b79e730f69f132460
unknown <asad.hnk@gmail.com>
2022-10-03 12:07:04 +0600

    starter template added

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      React starter template (typescript, eslint, prettier, husky,
+      airbnb-typescript-style-guide)
+    </div>
+  );
+}
+
+export default App;
